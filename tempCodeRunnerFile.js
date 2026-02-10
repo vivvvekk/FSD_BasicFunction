d23a272a64de88508bbc02fa213bf064e14e8803
vivvvekk <vivekkumar83402@gmail.com>
2026-02-10 12:04:41 +0530

    none

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+console.log(os.platform());   // win32, darwin, linux
+// console.log(os.arch());       // x64
+// console.log(os.totalmem());   // Total RAM
+// console.log(os.freemem());    // Free RAM
+// console.log(os.cpus().length); // CPU cores
+
+// Hashing a password using crypto module
